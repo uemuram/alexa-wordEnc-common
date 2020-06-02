e19abe0e9d5bfd6c6f594c91ce3ea78dd6e1e265
uemuram <uemura.masaru1124@gmail.com>
2020-06-02 22:56:40 +0900

    セッションへの値保存関数追加

--- layer/CommonUtil.js
+++ layer/CommonUtil.js
@@ -4,22 +4,30 @@ class CommonUtil {
 
     // 状態をチェック
     checkState(h, state) {
-        //    const attr = h.attributesManager.getSessionAttributes();
-        //    return (attr.STATE == state);
         return (this.getState(h) == state);
     }
 
     //状態を取得
     getState(h) {
-        const attr = h.attributesManager.getSessionAttributes();
-        return attr.STATE;
+        return this.getSessionValue(h, 'STATE');
     }
 
     //状態を保存
     setState(h, state) {
+        this.setSessionValue(h, 'STATE', state);
+    }
+
+    // セッションから値を取得
+    getSessionValue(h, key) {
+        const attr = h.attributesManager.getSessionAttributes();
+        return attr[key];
+    }
+
+    // セッションに値を入れる
+    setSessionValue(h, key, value) {
         let attr = h.attributesManager.getSessionAttributes();
-        attr.STATE = state
-        h.attributesManager.setSessionAttributes(attr);
+        attr[key] = value
+        h.attributesManager.setSessionAttributes(attr);        
     }
 
     getHello1() {
