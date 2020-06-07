5068ff67cbed9a3d071900a71e0802073e627e43
uemuram <uemura.masaru1124@gmail.com>
2020-06-07 22:31:30 +0900

    暗号化クラスを追加

--- layer/CommonUtil.js
+++ layer/CommonUtil.js
@@ -27,8 +27,25 @@ class CommonUtil {
     setSessionValue(h, key, value) {
         let attr = h.attributesManager.getSessionAttributes();
         attr[key] = value
-        h.attributesManager.setSessionAttributes(attr);        
+        h.attributesManager.setSessionAttributes(attr);
     }
+
+    // 指定されたメッセージを暗号化する
+    encrypt(key, message) {
+        console.log("<暗号化実施> [鍵:" + key + "][メッセージ:" + message + "]");
+        const Random = require('/opt/Random');
+        const random = new Random(key);
+
+        for (let i = 0; i < 10; i++) {
+            const value = random.nextInt(1, 10);
+            console.log(value);
+        }
+
+
+
+        return ["aaa", "bbb", "ccc"];
+    }
+
 
     getHello1() {
         return 'test5';
---
+++ layer/Random.js
@@ -0,0 +1,26 @@
+class Random {
+    constructor(seed) {
+        this.x = 123456789;
+        this.y = 362436069;
+        this.z = 521288629;
+        // 種として0が指定される場合を想定して加算しておく
+        this.w = 100000000 + seed;
+    }
+
+    // XorShift
+    next() {
+        let t;
+
+        t = this.x ^ (this.x << 11);
+        this.x = this.y; this.y = this.z; this.z = this.w;
+        return this.w = (this.w ^ (this.w >>> 19)) ^ (t ^ (t >>> 8));
+    }
+
+    // min以上max以下の乱数を生成する
+    nextInt(min, max) {
+        const r = Math.abs(this.next());
+        return min + (r % (max + 1 - min));
+    }
+}
+
+module.exports = Random;
